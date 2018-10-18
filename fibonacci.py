b9e02ed30e2ecc0a574e973c83dcb8244ca7ee41
kemypatel <41269124+kemypatel@users.noreply.github.com>
2018-10-18 16:55:09 +0530

    Python challenge for beginners
    Python challenge for beginners

---
+++ fibonacci.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 18 16:39:27 2018
+
+@author: kemy
+"""
+
+def recur_fibo(n):
+   """Recursive function to
+   print Fibonacci sequence"""
+   if n <= 1:
+       return n
+   else:
+       return(recur_fibo(n-1) + recur_fibo(n-2))
+
+nterms = 10
+
+if nterms <= 0:
+   print("Plese enter a positive integer")
+else:
+   print("Fibonacci sequence:")
+   for i in range(nterms):
+       print(recur_fibo(i))
